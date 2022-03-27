4fa2d25e63a40ea791732f08c8c66bbf730ea8c2
Zhiyuan Guo <zhiyuang2007@gmail.com>
2022-03-27 02:15:05 -0700

    Test

--- Test.js
+++ Test.js
@@ -1,11 +1,47 @@
-test = [
-    [3, 4],
-    [2, 3],
-    [1, 2]
-]
+var points = [
+     {x: 0, y: 0},
+     {x: 0, y: 5},
+     {x: 5, y: 0},
+     {x: 5, y: 5},
+     {x: 3, y: 3}
+];
 
-test.sort((x, y) => {
-    return x;
+/**
+ * @param {[x, y]} a
+ * @param {[x, y]} b
+ * @param {[x, y]} c
+ * @return {Boolean} True if right turn, false if left turn
+ */
+function rightTurn(a, b, c) {
+	return ((b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x)) <= 0;
+}
+
+function slope(p1, p2) {
+    return((p1.y - p2.y)/[p1.x - p2.x])
+}
+
+points.sort((a, b) => {
+    console.log(a.x, b.x);
+    return a.x - b.x;
+});
+
+hull = [points.shift()];
+
+
+points.sort((a, b) => {
+    return -(slope(b, hull[0]) - slope(a, hull[0]));
 })
 
-console.log(test)
+
+console.log(points);
+
+for (i = 0; i < points.length; i++) {
+    hull.push(points[i]);
+    while (hull.length > 2 && rightTurn(hull[hull.length - 3], hull[hull.length - 2], hull[hull.length - 1])) {
+        hull.splice(hull.length - 2, 1);
+    }
+}
+
+
+console.log(hull);
+
